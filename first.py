7c3b36838227fa3080b559e6bdff8f3a60cacf30
Surzhenko Matvey <76889438+surmaw3@users.noreply.github.com>
2023-09-27 16:22:56 +0300

    Update first.py

--- first.py
+++ first.py
@@ -1,42 +1,51 @@
-# Первый вариант решения на python
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2  # Находим середину массива
+        merge_sort(arr, left, mid)  # Сортируем левую половину
+        merge_sort(arr, mid + 1, right)  # Сортируем правую половину
+        merge(arr, left, mid, right)  # Сливаем отсортированные половины
 
-def merge(A, p, q, r):
-    n1 = q - p + 1
-    n2 = r - q
-
-    left = [0] * (n1 + 1)
-    right = [0] * (n2 + 1)
-
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+    
+    # Создаем временные массивы для левой и правой половин
+    left_half = [0] * (n1)
+    right_half = [0] * (n2)
+    
+    # Копируем данные во временные массивы left_half и right_half
     for i in range(n1):
-        left[i] = A[p + i]
+        left_half[i] = arr[left + i]
     for j in range(n2):
-        right[j] = A[q + j + 1]
-
-    left[n1] = float('inf')
-    right[n2] = float('inf')
-
+        right_half[j] = arr[mid + 1 + j]
+    
+    # Сливаем временные массивы обратно в основной массив arr
     i = 0
     j = 0
-    for k in range(p, r + 1):
-        if left[i] <= right[j]:
-            A[k] = left[i]
+    k = left
+    
+    while i < n1 and j < n2:
+        if left_half[i] <= right_half[j]:
+            arr[k] = left_half[i]
             i += 1
         else:
-            A[k] = right[j]
+            arr[k] = right_half[j]
             j += 1
+        k += 1
+    
+    # Дополняем оставшиеся элементы, если такие есть
+    while i < n1:
+        arr[k] = left_half[i]
+        i += 1
+        k += 1
+    
+    while j < n2:
+        arr[k] = right_half[j]
+        j += 1
+        k += 1
 
-
-def merge_sort(A, p, r):
-    if p < r:
-        q = (p + r) // 2
-        merge_sort(A, p, q)
-        merge_sort(A, q + 1, r)
-        merge(A, p, q, r)
-
-
-# Пример массива
-A = [5, 2, 4, 6, 1, 3, 2, 6]
-n = len(A)
-merge_sort(A, 0, n - 1)
-print(A)
-
+# Пример выполнения функции
+my_array = [5, 2, 4, 6, 1, 3, 2, 6]
+n = len(my_array)
+merge_sort(my_array, 0, n - 1)
+print(my_array)
